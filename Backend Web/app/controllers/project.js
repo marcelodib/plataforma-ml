cf3683e526b130f36c199f73c064d8287c063d4c
marcelodib <marcelocdmcdm@hotmail.com>
2020-05-26 18:29:18 -0300

    Add Project APIs

---
+++ Backend Web/app/controllers/project.js
@@ -0,0 +1,242 @@
+/*===============================IMPORT MODULES===============================*/
+const empty = require('is-empty');          /*Modulo responsável por fazer a verificação dos dados retornados pelo banco.*/
+const {errorLog} = require("../utils/log"); /*Modulo responsável por gerar log de eventos de erro.*/
+/*============================================================================*/
+
+/*============================PROJECT CONTROLLERS=============================*/
+
+/*===============================CREATE PROJECT===============================*/
+/**
+ * ================================================================
+ * |Controller createProject responsável por verificar se existe  |
+ * |sessão aberta para acessar esse controller.                   |
+ * |Caso as condições sejam verdadeiras, é realizado a inserção   |
+ * |do novo projeto na base de dados.                             |
+ * ================================================================
+ */
+module.exports.createProject = function (app, req, res) {
+    /*Atribuição da função isValid para validação do token.*/
+    const isValid = app.app.controllers.sign.isValid;
+    /*Verificação se o usuário possui permissão para acessar essa rota.*/
+    if (req.session.token != undefined && 
+        isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {      
+        /*Atribuição dos dados enviados no corpo da requisição.*/
+        const project = req.body;
+
+        /*Chamada da função que torna o objeto user imutável.*/
+        Object.freeze(project);
+        
+        /*Abertura de conexão com o banco de dados.*/
+        const connection = app.config.dbConnection();
+        const model = new app.app.models.project(connection);
+
+        /*Chamada do model que executa a query de inserção de um novo projeto na base de dados.*/
+        model.createProject(project, app, req, res, function (error, result) {
+
+            /*Verificação de erro no retorno do banco de dados.*/
+            if (error) {
+                errorLog(req.session.userEmail, "createProject", "createProject", "Erro ao acessar a base de dados para inserir novo projeto.", error);
+                res.send({  
+                    status: "error",
+                    msg   : "Erro ao acessar a base de dados para inserir novo projeto!",
+                    data  : error
+                });
+                return;
+            }
+            /*Verificação se o retorno do banco de dados está vazio.*/
+            else if (empty(result) || result.affectedRows == 0) {
+                errorLog(req.session.userEmail, "createProject", "createProject", "Projeto não foi inserido.", result);
+                res.send({
+                    status: "none",
+                    msg   : "Projeto não foi criado!"
+                });
+                return;
+            } else {
+
+                /*Envio da respostas*/
+                res.send({
+                    status: "success",
+                    msg   : "Novo projeto criado com sucesso!"
+                });
+                return;
+            }
+        });
+    } 
+    else {
+        /*Envio da respostas.*/
+        res.send({status: "error", msg: "Acesso Negado!"});
+        return;
+    }
+}
+/*============================================================================*/
+
+/*================================LIST PROJECT================================*/
+/**
+ * ================================================================
+ * |Controller listProject responsável por verificar se existe    |
+ * |sessão aberta para acessar esse controller.                   |
+ * |Caso as condições sejam verdadeiras, é verificado os dados    |
+ * |enviados e realizado a busca pelos projetos requisitadas.     |
+ * ================================================================
+ */
+module.exports.listProject = function (app, req, res) {
+
+    /*Atribuição da função isValid para validação do token.*/
+    const isValid = app.app.controllers.sign.isValid;
+    /*Verificação se o usuário possui permissão para acessar essa rota.*/
+    if (req.session.token != undefined && 
+        isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {      
+        
+        const idProject = req.body.idProject;
+
+        /*Chamada da função que torna o objeto user imutável.*/
+        Object.freeze(idProject);
+
+		/*Abertura de conexão com o banco de dados*/
+		const connection = app.config.dbConnection();
+		const model = new app.app.models.project(connection);
+
+		/*Chamada da função que executa a query de busca de projetos na base de dados*/
+		model.listProject(idProject, app, req, res, function (error, result) {
+
+			/*Verificação de erro no retorno do banco de dados*/
+			if (error) {
+                errorLog(req.session.userEmail, "listProject", "listProject", "Erro ao acessar a base de dados para buscar projetos.", error);
+				res.send({
+                    status: "error",
+                    msg: "Erro ao acessar a base de dados para buscar projetos!",
+                    data: error
+                });
+				return;
+			}
+			/*Verificação se o retorno do banco de dados está vazio*/
+			else if (empty(result)) {
+                errorLog(req.session.userEmail, "listProject", "listProject", "Nenhum projeto foi encontrado.", result);
+				res.send({  
+                    status: "none",
+                    msg: "Nenhum projeto foi encontrado!"
+                });
+				return;
+			} else {
+				/*Envio da respostas*/
+				res.send({  
+                    status: "success",
+                    msg: "Projetos encontradas com sucesso!",
+                    data: result
+                });
+				return;
+			}
+		});
+	} else {
+		/*Envio da respostas*/
+		res.send({status: "error", msg: "Acesso Negado!"});
+		return;
+	}
+}
+/*============================================================================*/
+
+/*===============================DELETE PROJECT===============================*/
+/**
+ * ================================================================
+ * |Controller deleteProject responsável por verificar se existe  |
+ * |sessão aberta para acessar esse controller.                   |
+ * |Caso as condições sejam verdadeiras, é verificado os dados    |
+ * |enviados e realizado a remoção do projeto requisitado.        |
+ * ================================================================
+ */
+module.exports.deleteProject = function (app, req, res) {
+
+    /*Atribuição da função isValid para validação do token.*/
+    const isValid = app.app.controllers.sign.isValid;
+    /*Verificação se o usuário possui permissão para acessar essa rota.*/
+    if (req.session.token != undefined && 
+        isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {      
+        
+        const idProject = req.body.idProject;
+
+        /*Chamada da função que torna o objeto user imutável.*/
+        Object.freeze(project);
+
+		/*Abertura de conexão com o banco de dados*/
+		const connection = app.config.dbConnection();
+		const model = new app.app.models.project(connection);
+
+		/*Chamada da função que executa a query de remoção do projeto requisitado na base de dados*/
+		model.deleteProject(idProject, app, req, res, function (error, result) {
+
+			/*Verificação de erro no retorno do banco de dados*/
+			if (error) {
+                errorLog(req.session.userEmail, "deleteProject", "deleteProject", "Erro ao acessar a base de dados para remover projeto.", error);
+				res.send({
+                    status: "error",
+                    msg: "Erro ao acessar a base de dados para remover projeto!",
+                    data: error
+                });
+				return;
+			}
+			/*Verificação se o retorno do banco de dados está vazio*/
+			else if (empty(result) || result.affectedRows == 0) {
+                errorLog(req.session.userEmail, "deleteProject", "deleteProject", "Nenhum projeto foi removido.", result);
+				res.send({
+                    status: "none",
+                    msg: "Nenhum projeto foi removido!"
+                });
+				return;
+			} else {
+				/*Envio da respostas*/
+				res.send({
+                    status: "success",
+                    msg: "Projeto removido com sucesso!",
+                    data: result
+                });
+				return;
+			}
+		});
+	} else {
+		/*Envio da respostas*/
+		res.send({status: "error", msg: "Acesso Negado!"});
+		return;
+	}
+}
+/*============================================================================*/
+
+/*============================UPDATE STATUS PROJECT===========================*/
+/**
+ * ================================================================
+ * |Controller updateStatusProject responsável por realizar a     |
+ * |atualização do status do projeto requisitado.                 |
+ * ================================================================
+ */
+module.exports.updateStatusProject = function (project, app, req) {
+
+    /*Chamada da função que torna o objeto user imutável.*/
+    Object.freeze(project);
+
+    /*Abertura de conexão com o banco de dados*/
+    const connection = app.config.dbConnection();
+    const model = new app.app.models.project(connection);
+    
+    return new Promise((resolve,reject) => {
+        /*Chamada da função que executa a query de remoção do projeto requisitado na base de dados*/
+        model.updateStatusProject(project, function (error, result) {
+
+            /*Verificação de erro no retorno do banco de dados*/
+            if (error) {
+                errorLog(req.session.userEmail, "updateStatusProject", "updateStatusProject", "Erro ao acessar a base de dados para atualizar status do projeto.", error);
+                resolve(false);
+            }
+            /*Verificação se o retorno do banco de dados está vazio*/
+            else if (empty(result) || result.affectedRows == 0) {
+                errorLog(req.session.userEmail, "updateStatusProject", "updateStatusProject", "Nenhum projeto foi atualizado.", result);
+                resolve(false);
+            } else {
+                /*Envio da respostas*/
+                resolve(true);
+            }
+        });
+    });
+}
+/*============================================================================*/
+
+/*============================================================================*/
+
---
+++ Backend Web/app/routes/project.js
@@ -0,0 +1,251 @@
+/*===============================IMPORT MODULES===============================*/
+const { check, validationResult } = require('express-validator'); /*Modulo responsável por fazer a validação dos dados que chegam nas requisições.*/
+const multer = require('multer');
+/*============================================================================*/
+
+/*================================USER ROUTES=================================*/
+module.exports = function (app) {
+
+/*===============================CREATE PROJECT===============================*/
+    /** 
+     * =======================================================================
+     * |Route createProject responsável por verificar se o usuário possuí    |
+     * |sessão aberta para acessar essa rota.                                |
+     * |Caso as condições sejam verdadeiras, retorna a pagina de criação de  |
+     * |projeto, caso contrário, retorna a pagina de sign In.                |
+     * =======================================================================
+    */
+    app.get('/createProject', function (req, res) {
+        /*Atribuição da função isValid para validação do token.*/
+        const isValid = app.app.controllers.sign.isValid;
+        /*Verificação se o usuário possui permissão para acessar essa rota.*/
+        if (req.session.token != undefined && 
+            isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+            /*Renderiza tela de criação de projeto.*/
+            res.render("./project/createProject");
+            return;
+        }
+        else{
+            /*Redirecionamento para página de sigIn, pois não possui permissão de acesso.*/
+            res.redirect("/signIn");
+            return;
+        }
+    });
+
+    /** 
+     * =======================================================================
+     * |Route createProject responsável por verificar se o usuário possuí    |
+     * |sessão aberta para acessar essa rota.                                |
+     * |Caso as condições sejam verdadeiras, verifica se os dados enviados   |
+     * |são validos, e realiza o cadastro do projeto, caso contrario,        |
+     * |retorna o erro.                                                      |
+     * =======================================================================
+    */
+    app.post('/createProject', 
+        [
+            check('projectName', 'Nome inválido!')  .not().isEmpty().escape().isString().isLength({ max: 127 }),
+            check('className'  , 'Classe inválido!').not().isEmpty().escape().isString().isLength({ max: 63 }), 
+        ], function (req, res) {
+                /*Atribuição da função isValid para validação do token.*/
+                const isValid = app.app.controllers.sign.isValid;
+                /*Verificação se o usuário possui permissão para acessar essa rota.*/
+                if (req.session.token != undefined && 
+                    isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+                    /*Chamada da função que valida os dados da requisição.*/
+                    const errors = validationResult(req)
+                    /*Verificação se os parâmetros não apresentam inconsistências.*/            
+                    if (!errors.isEmpty()) {
+                        /*Envio da respostas.*/
+                        res.send({status: "error", msg: errors.array()});
+                        return;
+                    }
+                    else {
+                        /*Chamada do controller parar realizar a inserção do novo projeto.*/
+                        app.app.controllers.project.createProject(app, req, res);
+                    }
+                }
+                else {
+                    /*Envio da respostas.*/
+                    res.send({status: "error", msg: "Acesso Negado!"});
+                    return;
+                }
+            }
+        );
+/*============================================================================*/
+
+/*=================================LIST PROJECT===============================*/
+    /** 
+     * =======================================================================
+     * |Route listProject responsável por verificar se o usuário possuí      |
+     * |sessão aberta para acessar essa rota.                                |
+     * |Caso as condições sejam verdadeiras, retorna a pagina de listagem de |
+     * |projetos, caso contrário, retorna a pagina de sign In.               |
+     * =======================================================================
+    */
+    app.get('/listProject', function (req, res) {
+        /*Atribuição da função isValid para validação do token.*/
+        const isValid = app.app.controllers.sign.isValid;
+        /*Verificação se o usuário possui permissão para acessar essa rota.*/
+        if (req.session.token != undefined && 
+            isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+            /*Renderiza tela de listagem de projetos.*/
+            res.render("./project/listProject");
+            return;
+        }
+        else{
+            /*Redirecionamento para página de sigIn, pois não possui permissão de acesso.*/
+            res.redirect("/signIn");
+            return;
+        }
+    });
+
+    /** 
+     * ========================================================================
+     * |Route listProject responsável por verificar se o usuário possuí       |
+     * |sessão aberta para acessar essa rota.                                 |
+     * |Caso as condições sejam verdadeiras, verifica se os dados enviados    |
+     * |são validos, e realiza a busca dos dados projetos requisitados,       |
+     * |caso contrario, retorna o erro.                                       |
+     * ========================================================================
+    */
+    app.post('/listProject', [check('idProject', 'Id do projeto inválido').isInt()], 
+    function (req, res) {
+        /*Atribuição da função isValid para validação do token.*/
+        const isValid = app.app.controllers.sign.isValid;
+        /*Verificação se o usuário possui permissão para acessar essa rota.*/
+        if (req.session.token != undefined && 
+            isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+            /*Chamada da função que valida os dados da requisição.*/
+            const errors = validationResult(req)
+            /*Verificação se os parâmetros não apresentam inconsistências.*/            
+            if (!errors.isEmpty()) {
+                /*Envio da respostas.*/
+                res.send({status: "error", msg: errors.array()});
+                return;
+            }
+            else {
+                /*Chamada do controller parar realizar a busca dos projetos.*/
+                app.app.controllers.project.listProject(app, req, res);
+            }
+        }
+        else {
+            /*Envio da respostas.*/
+            res.send({status: "error", msg: "Acesso Negado!"});
+            return;
+        }
+    });
+/*============================================================================*/
+
+/*===============================DELETE PROJECT===============================*/
+    /** 
+     * ==========================================================================
+     * |Route deleteProject responsável por verificar se o usuário possuí       |
+     * |sessão aberta para acessar essa rota.                                   |
+     * |Caso as condições sejam verdadeiras, verifica se os dados enviados      |
+     * |são validos, e deleta o projeto, caso contrario, retorna o erro.        |
+     * ==========================================================================
+    */
+    app.post('/deleteProject', [check('idProject', 'Id do Projeto Inválido!').not().isEmpty().escape().isInt()], 
+    function (req, res) {
+        /*Atribuição da função isValid para validação do token.*/
+        const isValid = app.app.controllers.sign.isValid;
+        /*Verificação se o usuário possui permissão para acessar essa rota.*/
+        if (req.session.token != undefined && 
+            isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+            /*Chamada da função que valida os dados da requisição.*/
+            const errors = validationResult(req)
+            /*Verificação se os parâmetros não apresentam inconsistências.*/            
+            if (!errors.isEmpty()) {
+                /*Envio da respostas.*/
+                res.send({status: "error", msg: errors.array()});
+                return;
+            }
+            else {
+                /*Chamada do controller para deletar um determinado projeto.*/
+                app.app.controllers.project.deleteProject(app, req, res);
+            }
+        }
+        else {
+            /*Envio da respostas.*/
+            res.send({status: "error", msg: "Acesso Negado!"});
+            return;
+        }
+    });
+/*============================================================================*/
+
+/*================================UPLOAD DATASET==============================*/
+    /** 
+     * =======================================================================
+     * |Route uploadDataset responsável por verificar se o usuário possuí    |
+     * |sessão aberta.                                                       |
+     * |Caso as condições sejam verdadeiras, verifica o dataset enviado e    |
+     * |armazena-o na pasta do projeto.                                      |
+     * =======================================================================
+    */
+    app.post('/uploadDataset', function(req, res){
+        /*Atribuição da função isValid para validação do token.*/
+        const isValid = app.app.controllers.sign.isValid;
+        /*Verificação se o usuário possui permissão para acessar essa rota.*/
+        if (req.session.token != undefined && 
+            isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+                const storage = multer.diskStorage({
+                    destination: function (req, file, callback) {
+                        callback(null, './uploads');
+                    },
+                    filename: function (req, file, callback) {
+                        callback(null, file.fieldname + '-' + Date.now());
+                    }
+                });
+                const upload = multer({ storage : storage}).single('userPhoto');
+                
+                upload(req,res,function(err) {
+                    if(err) {
+                        return res.end("Error uploading file.");
+                    }
+                    res.end("File is uploaded");
+                });
+                
+            return;
+        } 
+        else {
+            /*Envio da resposta.*/
+            res.send({status: "error", msg: "Acesso Negado!"});
+            return;
+        }
+    });
+/*============================================================================*/
+
+/*===============================DOWNLOAD MODEL===============================*/
+    /** 
+     * =======================================================================
+     * |Route downloadModel responsável por verificar se o usuário possuí    |
+     * |sessão aberta.                                                       |
+     * |Caso as condições sejam verdadeiras, retorna o model requisitado     |
+     * |pelo usuário.                                                        |
+     * =======================================================================
+    */
+    app.get('/downloadModel', function(req, res){
+        /*Atribuição da função isValid para validação do token.*/
+        const isValid = app.app.controllers.sign.isValid;
+        /*Verificação se o usuário possui permissão para acessar essa rota.*/
+        if (req.session.token != undefined && 
+            isValid(req.session.userEmail + req.session.userName + req.session.idUser.toString(), req.session.token)) {
+            
+            /*Atribuição do path do arquivo passado como parâmetro na requisição.*/
+            const idProject   = req.query.idProject;
+
+            const path = "./users/" + req.session.userEmail + "/projects/" + idProject + "/models/model.tflite" 
+
+            res.download(path);
+            return;
+        } 
+        else {
+            /*Envio da resposta.*/
+            res.send({status: "error", msg: "Acesso Negado!"});
+            return;
+        }
+    });
+/*============================================================================*/
+
+};
+/*============================================================================*/
