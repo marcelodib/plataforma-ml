561420dc95454a93fdc0d0301fc5123b012328bb
marcelodib <marcelocdmcdm@hotmail.com>
2020-05-29 19:55:19 -0300

    Add functions to call .sh files

--- Backend Web/app/utils/shell.js
+++ Backend Web/app/utils/shell.js
@@ -1,27 +1,47 @@
+module.exports.unzip = function () {
+    const util = require('util');
+
+    const exec = util.promisify(require('child_process').exec);
+
+    const { stderr } = await exec('sh ~/Desktop/ECCNNO/Machine Learning/shellScripts/unzip.sh');
+
+    if (stderr) {
+        console.error(`error: ${stderr}`);
+    }
+}
 
 module.exports.startTrain = async function () {
     const util = require('util');
 
     const exec = util.promisify(require('child_process').exec);
 
-    const { stdout, stderr } = await exec('sh train.sh');
+    const { stderr } = await exec('sh ~/Desktop/ECCNNO/Machine Learning/shellScripts/train.sh');
 
     if (stderr) {
         console.error(`error: ${stderr}`);
     }
-    console.log(`Number of files ${stdout}`);
-}
-
-module.exports.stopTrain = function () {
-    if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-        shell.echo('Error: Git commit failed');
-        shell.exit(1);
-    }
 }
 
 module.exports.convertTflite = function () {
-    if (shell.exec('git commit -am "Auto-commit"').code !== 0) {
-        shell.echo('Error: Git commit failed');
-        shell.exit(1);
+    const util = require('util');
+
+    const exec = util.promisify(require('child_process').exec);
+
+    const { stderr } = await exec('sh ~/Desktop/ECCNNO/Machine Learning/shellScripts/convertTflite.sh');
+
+    if (stderr) {
+        console.error(`error: ${stderr}`);
+    }
+}
+
+module.exports.exportGraph = function () {
+    const util = require('util');
+
+    const exec = util.promisify(require('child_process').exec);
+
+    const { stderr } = await exec('sh ~/Desktop/ECCNNO/Machine Learning/shellScripts/exportGraph.sh');
+
+    if (stderr) {
+        console.error(`error: ${stderr}`);
     }
 }
