b618f86835a9199248de619cfa896a2014d9a751
marcelodib <marcelocdmcdm@hotmail.com>
2020-05-31 18:00:41 -0300

    Rename Machine Learning dir

---
+++ Machine_Learning/shellScripts/mkdirProject.sh
@@ -0,0 +1,16 @@
+cd ~/Desktop/ECCNNO/Users/$1/projects/
+
+mkdir $2
+
+cd ~/Desktop/ECCNNO/Users/$1/projects/$2/
+
+mkdir dataset
+
+mkdir config
+
+mkdir model
+
+mkdir graph
+
+mkdir tflite
+
---
+++ Machine_Learning/shellScripts/mkdirUser.sh
@@ -0,0 +1,7 @@
+cd ~/Desktop/ECCNNO/Users/
+
+mkdir $1
+
+cd ~/Desktop/ECCNNO/Users/$1/
+
+mkdir projects
