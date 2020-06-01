469d3a0385db8bfa0dbca775cd1ba5ebf5ccc3cc
marcelodib <marcelocdmcdm@hotmail.com>
2020-05-31 23:16:00 -0300

    Add call xml to csv and generate tfrecord

--- Machine_Learning/shellScripts/train.sh
+++ Machine_Learning/shellScripts/train.sh
@@ -6,5 +6,20 @@ export PYTHONPATH=$PYTHONPATH:`pwd`:`pwd`/slim
 
 cd ~/Desktop/models/research/object_detection/legacy/
 
-python3 train.py --logtostderr --train_dir=training/ --pipeline_config_path=training/ssd_mobilenet_v2_quantized_300x300_coco.config
+python3 xml_to_csv.py "/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/"
+
+python3 generate_tfrecord.py "$3" \
+--csv_input=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/train_labels.csv \
+--output_path=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/train.record \
+--image_dir=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/dataset/train
+
+python3 generate_tfrecord.py "$3" \
+--csv_input=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/test_labels.csv \
+--output_path=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/test.record \
+--image_dir=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/dataset/test
+
+python3 train.py \
+--logtostderr \
+--train_dir=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/model/ \
+--pipeline_config_path=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/trainConfig.config
 
