3d3b11bb40eb86b6751a9184e5190ff404256338
marcelodib <marcelocdmcdm@hotmail.com>
2020-06-03 16:58:38 -0300

    Add call export graph AND convert Tflite

--- Machine_Learning/scripts/train.sh
+++ Machine_Learning/scripts/train.sh
@@ -23,3 +23,26 @@ python3 train.py \
 --train_dir=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/model/ \
 --pipeline_config_path=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/trainConfig.config
 
+cd ~/Desktop/models/research/object_detection/
+
+python3 export_tflite_ssd_graph.py \
+--input_type=image_tensor \
+--pipeline_config_path=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/config/trainConfig.config \
+--trained_checkpoint_prefix=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/model/model.ckpt-1000 \
+--output_directory=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/graph \
+--add_postprocessing_op=true \
+--max_detections 10
+
+tflite_convert \
+--output_file=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/tflite/detect.tflite \
+--graph_def_file=/home/marcelo/Desktop/ECCNNO/Users/$1/projects/$2/graph/tflite_graph.pb \
+--output_format=TFLITE \
+--input_arrays=normalized_input_image_tensor \
+--input_shapes=1,300,300,3 \
+--inference_type=QUANTIZED_UINT8 \
+--mean_values=128 \
+--std_dev_values=128 \
+--change_concat_input_ranges=false \
+--output_arrays='TFLite_Detection_PostProcess','TFLite_Detection_PostProcess:1','TFLite_Detection_PostProcess:2','TFLite_Detection_PostProcess:3' \
+--allow_custom_ops
+
