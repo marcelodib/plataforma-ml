1460b0bdbc7f0467fc19099bcc6f98f3e4acf381
marcelodib <marcelocdmcdm@hotmail.com>
2020-08-03 19:32:21 -0300

    Fix model path

--- machine-learning/scripts/train.sh
+++ machine-learning/scripts/train.sh
@@ -28,7 +28,7 @@ cd ~/Desktop/models/research/object_detection/
 python3 export_tflite_ssd_graph.py \
 --input_type=image_tensor \
 --pipeline_config_path=/home/marcelo/Desktop/plataforma-ml/Users/$1/projects/$2/config/trainConfig.config \
---trained_checkpoint_prefix=/home/marcelo/Desktop/plataforma-ml/Users/$1/projects/$2/model/model.ckpt-1000 \
+--trained_checkpoint_prefix=/home/marcelo/Desktop/plataforma-ml/Users/$1/projects/$2/model/model.ckpt-100 \
 --output_directory=/home/marcelo/Desktop/plataforma-ml/Users/$1/projects/$2/graph \
 --add_postprocessing_op=true \
 --max_detections 10
