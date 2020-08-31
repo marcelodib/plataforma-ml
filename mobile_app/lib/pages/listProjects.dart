34e972fa6205c6cf777e26c5753fc5dd54a220bc
marcelodib <marcelocdmcdm@hotmail.com>
2020-08-31 00:01:42 -0300

    Add list projects dynamic

--- mobile_app/lib/pages/listProjects.dart
+++ mobile_app/lib/pages/listProjects.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:plataforma_ml/api/project.dart';
+import 'package:plataforma_ml/pages/login.dart';
 import 'package:plataforma_ml/pages/project.dart';
 import 'package:plataforma_ml/utils/colors.dart';
+import 'package:plataforma_ml/widgets/circularProgress.dart';
 import 'package:plataforma_ml/widgets/customCard.dart';
 
 class ListProjectsPage extends StatefulWidget {
@@ -9,10 +13,38 @@ class ListProjectsPage extends StatefulWidget {
 }
 
 class _ListProjectsPageState extends State<ListProjectsPage> {
+  List projects = [];
+
+  @override
+  initState() {
+    SystemChrome.setEnabledSystemUIOverlays([]);
+    super.initState();
+    listProject().then((value) => {
+          setState(() {
+            projects = value;
+          })
+        });
+  }
+
+  updateListProject() async {
+    List aux = await listProject();
+    setState(() {
+      projects = aux;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: primaryOrange,
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async => {await updateListProject()},
+        backgroundColor: primaryOrange,
+        child: Icon(
+          Icons.update,
+          size: 30,
+        ),
+      ),
       body: Stack(
         children: <Widget>[
           Column(
@@ -39,7 +71,11 @@ class _ListProjectsPageState extends State<ListProjectsPage> {
                           color: Colors.white,
                         ),
                         onPressed: () {
-                          // Navigator.pop(context);
+                          Navigator.pushAndRemoveUntil(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => LoginPage()),
+                              (route) => false);
                         },
                       ),
                     ],
@@ -64,127 +100,41 @@ class _ListProjectsPageState extends State<ListProjectsPage> {
             mainAxisSize: MainAxisSize.min,
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              SizedBox(height: MediaQuery.of(context).size.height / 6.5),
-              CircularProgress(),
-              SizedBox(
-                height: 21,
+              SizedBox(height: MediaQuery.of(context).size.height / 12),
+              Center(
+                child: Text(
+                  "Meus Projetos",
+                  style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 30,
+                      fontWeight: FontWeight.w700),
+                ),
               ),
+              SizedBox(height: MediaQuery.of(context).size.height / 35),
+              CircularProgress(100, [
+                Image.asset("logo-orange.png"),
+              ]),
               Expanded(
-                  child: ListView(
-                children: [
-                  GestureDetector(
-                    onTap: () => Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => ProjectPage())),
+                  child: ListView.builder(
+                itemCount: projects.length,
+                itemBuilder: (context, index) {
+                  return GestureDetector(
+                    onTap: () => Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => ProjectPage(
+                                projects[index]["idProject"],
+                                projects[index]["projectName"],
+                                projects[index]["className"]))),
                     child: CustomCard(
-                      title: "Projeto 1",
-                      description: "Placa",
+                      title: "${projects[index]["projectName"]}",
+                      description: "${projects[index]["className"]}",
                       icon: Icons.bubble_chart,
                     ),
-                  ),
-                  GestureDetector(
-                    child: CustomCard(
-                      title: "Projeto 2",
-                      description: "Carro",
-                      icon: Icons.bubble_chart,
-                    ),
-                  ),
-                  GestureDetector(
-                    child: CustomCard(
-                      title: "Projeto 3",
-                      description: "Gato",
-                      icon: Icons.bubble_chart,
-                    ),
-                  ),
-                  GestureDetector(
-                    child: CustomCard(
-                      title: "Projeto 3",
-                      description: "Gato",
-                      icon: Icons.bubble_chart,
-                    ),
-                  ),
-                  GestureDetector(
-                    child: CustomCard(
-                      title: "Projeto 3",
-                      description: "Gato",
-                      icon: Icons.bubble_chart,
-                    ),
-                  ),
-                  GestureDetector(
-                    child: CustomCard(
-                      title: "Projeto 3",
-                      description: "Gato",
-                      icon: Icons.bubble_chart,
-                    ),
-                  ),
-                  GestureDetector(
-                    child: CustomCard(
-                      title: "Projeto 3",
-                      description: "Gato",
-                      icon: Icons.bubble_chart,
-                    ),
-                  ),
-                ],
+                  );
+                },
               ))
             ],
-          ),
-        ],
-      ),
-    );
-  }
-}
-
-class Test extends StatefulWidget {
-  @override
-  _TestState createState() => _TestState();
-}
-
-class _TestState extends State<Test> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: primaryOrange,
-      body: Column(
-        children: [
-          Flexible(
-            flex: 1,
-            child: Container(
-              height: double.infinity,
-              padding: EdgeInsets.all(15.0),
-              alignment: Alignment.topLeft,
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  begin: Alignment.topCenter,
-                  end: Alignment.bottomCenter,
-                  colors: [secondaryOrange, primaryOrange],
-                ),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  IconButton(
-                    icon: Icon(
-                      Icons.arrow_back,
-                      color: Colors.white,
-                    ),
-                    onPressed: () {
-                      Navigator.pop(context);
-                    },
-                  ),
-                ],
-              ),
-            ),
-          ),
-          Flexible(
-            flex: 3,
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.only(
-                  // topLeft: Radius.circular(30),
-                  topRight: Radius.circular(35),
-                ),
-              ),
-            ),
           ),
         ],
       ),
